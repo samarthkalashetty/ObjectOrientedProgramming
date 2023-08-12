3de08fbe16a4338a348b46ad8bbfa71b24242af6
samarthkalashetty <samarthkalashetti2612@gmail.com>
2023-08-12 17:04:23 +0530

    Welcome to PrintPattern Program

--- Program.cs
+++ Program.cs
@@ -1,55 +1,17 @@
 ﻿using System;
 
-class ElementAnalyzer
+class PatternPrinter
 {
-    private int[] array;
-    private int maximum;
-    private int minimum;
-    private bool analyzed;
-
-    public ElementAnalyzer(int[] inputArray)
+    public static void PrintPattern(int rows)
     {
-        array = inputArray;
-        analyzed = false;
-    }
-
-    public void AnalyzeElements()
-    {
-        if (array.Length == 0)
+        for (int i = rows; i >= 1; i--)
         {
-            Console.WriteLine("Array is empty.");
-            return;
-        }
-
-        maximum = array[0];
-        minimum = array[0];
-
-        foreach (int num in array)
-        {
-            if (num > maximum)
+            for (int j = 1; j <= i; j++)
             {
-                maximum = num;
+                Console.Write("*");
             }
-
-            if (num < minimum)
-            {
-                minimum = num;
-            }
+            Console.WriteLine();
         }
-
-        analyzed = true;
-    }
-
-    public void PrintResults()
-    {
-        if (!analyzed)
-        {
-            Console.WriteLine("Please analyze elements first.");
-            return;
-        }
-
-        Console.WriteLine($"Maximum element: {maximum}");
-        Console.WriteLine($"Minimum element: {minimum}");
     }
 }
 
@@ -57,18 +19,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Enter the number of elements: ");
-        int n = int.Parse(Console.ReadLine());
+        Console.Write("Enter the number of rows: ");
+        int rows = int.Parse(Console.ReadLine());
 
-        int[] array = new int[n];
-        for (int i = 0; i < n; i++)
-        {
-            Console.Write($"Enter element {i + 1}: ");
-            array[i] = int.Parse(Console.ReadLine());
-        }
-
-        ElementAnalyzer analyzer = new ElementAnalyzer(array);
-        analyzer.AnalyzeElements();
-        analyzer.PrintResults();
+        PatternPrinter.PrintPattern(rows);
     }
 }
